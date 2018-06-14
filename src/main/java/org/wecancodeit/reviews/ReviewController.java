029834f65286b9d9a59a1441ea818f24c2d1e383
Siddiqa <siddiqarasheed@gmail.com>
2018-06-14 00:30:14 -0400

    second

--- src/main/java/org/wecancodeit/reviews/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewController.java
@@ -14,7 +14,7 @@ public class ReviewController {
 	@RequestMapping("/show-reviews") //controller controlling end points where we're mapping to
 	public String findAllReviews(Model model) {
 		model.addAttribute("reviews", reviewRepo.findAll());
-		//(attribute name(model name), and attrivbuteValue(everything going to show on template)
+		//(attribute name(model name), and attributeValue(everything going to show on template)
 		return "reviews";
 	}
 }
